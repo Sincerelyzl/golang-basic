9b94036e1adb829e9d9611df9f07a16df9d5dcc6
Sincerelyzl <saklaw33@gmail.com>
2024-06-24 00:45:09 +0700

    6 func added

--- main.go
+++ main.go
@@ -1,15 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	// var sum int
-	// for i := 1; i <= 10; i++ {
-	// 	sum += i
-	// }
-	// fmt.Println(sum)
 
-	for i := range 10 {
-		fmt.Println(i)
+	a := 50
+	b := 10
+
+	// swap(&a, &b)
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+
+	fmt.Println("Max : ", max(a, b))
+	fmt.Println("Sum : ", sum(a, b))
+	fmt.Println("IsEven : ", isEven(a))
+	fmt.Println("divide : ", divide(a, b))
+	fmt.Println("power : ", power(a, b))
+
+}
+
+func swap(a, b *int) {
+	olda := *a
+	*a = *b
+	*b = olda
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
 	}
+	return b
+
+}
+
+func sum(a int, b int) int {
+	sum := a + b
+	return sum
+}
+
+func isEven(a int) bool {
+	if evenCheck := a % 2; evenCheck == 0 {
+		return true
+	}
+	return false
+}
+
+func divide(a int, b int) float64 {
+	divder := (float64(a)) / float64(b)
+	return divder
+}
+
+func power(base int, exponent int) int {
+	powerer := math.Pow(float64(base), float64(exponent))
+	return int(powerer)
 }
