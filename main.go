382fa75a707df6e7827987eb879c251f5378cc6f
Sincerelyzl <saklaw33@gmail.com>
2024-06-30 19:12:53 +0700

    finish

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"interface/shape"
+	"interface/creature"
 )
 
 func main() {
-	rectangle := shape.Rectangle{Width: 10, Length: 20}
-	triangle := shape.Triangle{Base: 10, Height: 20}
-	circle := shape.Circle{Radius: 10}
+	// rectangle := shape.Rectangle{Width: 10, Length: 20}
+	// triangle := shape.Triangle{Base: 10, Height: 20}
+	// circle := shape.Circle{Radius: 10}
+	// rectangle.Area()
+	// fmt.Println("Area of rectangle is : ", shape.CalArea(rectangle))
+	// fmt.Println("Area of triangle is : ", shape.CalArea(triangle))
+	// fmt.Println("Area of circle is : ", shape.CalArea(circle))
 
-	fmt.Println("Area of rectangle is : ", shape.CalArea(rectangle))
-	fmt.Println("Area of triangle is : ", shape.CalArea(triangle))
-	fmt.Println("Area of circle is : ", shape.CalArea(circle))
+	// shape.GetInfo(rectangle)
+	// shape.GetInfo(triangle)
+	// shape.GetInfo(circle)
+	intAdd := 5000
 
-	shape.GetInfo(rectangle)
-	shape.GetInfo(triangle)
-	shape.GetInfo(circle)
+	player1 := creature.NewPlayer(0.0)
+	player2 := creature.NewPlayer(intAdd)
+
+	// player1.ShowLifePoint()
+	// player2.ShowLifePoint()
+
+	creature.AttackEvent(&player1, &player2, 1000)
 
 }
