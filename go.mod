71b97bb63ec1723ab5361ec5f3af6bf5d63fc320
Sincerelyzl <saklaw33@gmail.com>
2024-06-30 19:26:00 +0700

    initialize project

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module interface
+module fiberhttp
 
 go 1.22.4
--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"interface/creature"
-)
-
 func main() {
-	// rectangle := shape.Rectangle{Width: 10, Length: 20}
-	// triangle := shape.Triangle{Base: 10, Height: 20}
-	// circle := shape.Circle{Radius: 10}
-	// rectangle.Area()
-	// fmt.Println("Area of rectangle is : ", shape.CalArea(rectangle))
-	// fmt.Println("Area of triangle is : ", shape.CalArea(triangle))
-	// fmt.Println("Area of circle is : ", shape.CalArea(circle))
-
-	// shape.GetInfo(rectangle)
-	// shape.GetInfo(triangle)
-	// shape.GetInfo(circle)
-	intAdd := 5000
-
-	player1 := creature.NewPlayer(0.0)
-	player2 := creature.NewPlayer(intAdd)
-
-	// player1.ShowLifePoint()
-	// player2.ShowLifePoint()
-
-	creature.AttackEvent(&player1, &player2, 1000)
 
 }
