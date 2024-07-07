857b51ccad0f86e77fede6f3308270cd08a454c7
Sincerelyzl <saklaw33@gmail.com>
2024-07-07 23:15:49 +0700

    finish FiberwithDatabase

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
 )
@@ -11,26 +13,95 @@ type Player struct {
 	PlayerLv   int    `json:"level"`
 }
 
+var Client *DatabaseController
+
 func main() {
 	// Connection string
-	client, err := ConnectDatabase()
+	var err error
+	Client, err = ConnectDatabase()
 	if err != nil {
 		panic(err)
 	}
-	defer client.Db.Close()
+	defer Client.Db.Close()
 
 	//FiberGO
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+
+	//API ZONE
+	app.Get("/player/:id", getPlayerHandler)
+	app.Post("/player", createPlayerHandler)
+	app.Put("/player/:id", updatePlayerHandler)
+	app.Delete("/player/:id", deletePlayerHandler)
+	app.Get("/player", getAllPlayerHandler)
+
 	app.Listen(":8080")
 }
 
 func getPlayerHandler(c *fiber.Ctx) error {
-	// playerId := c.Params("id")
-	// player, err := GetPlayer(playerId)
-	return nil
+	playerId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	player, err := Client.GetPlayer(playerId)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	return c.JSON(player)
+
 }
 
-//FuncZone
+func createPlayerHandler(c *fiber.Ctx) error {
+	p := new(Player)
+	if err := c.BodyParser(p); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	err := Client.CreatePlayer(p)
+
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	return c.JSON(p)
+}
+
+func updatePlayerHandler(c *fiber.Ctx) error {
+	playerId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	p := new(Player)
+	if err := c.BodyParser(p); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	player, err := Client.UpdatePlayer(playerId, p)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	return c.JSON(player)
+}
+
+func deletePlayerHandler(c *fiber.Ctx) error {
+	playerId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	err = Client.DeletePlayer(playerId)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	return c.SendStatus(fiber.StatusNoContent)
+
+}
+
+func getAllPlayerHandler(c *fiber.Ctx) error {
+	players, err := Client.GetMorePlayer()
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	return c.JSON(players)
+}
